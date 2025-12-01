9e4d2f45786c36b5375b6f6cafc51aea833dd25a
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-12-01 01:41:39 +0100

    feat(server/config): fail fast on missing env vars

--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -1,11 +1,25 @@
 export const ENV = {
   cors: {
-    CORS_ORIGIN: process.env.CORS_ORIGIN,
+    CORS_ORIGIN: getEnvOrPanic("CORS_ORIGIN"),
   },
   database: {
-    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
+    DB_CONNECTION_STRING: getEnvOrPanic("DB_CONNECTION_STRING"),
   },
   server: {
-    PORT: process.env.PORT,
+    PORT: getEnvOrPanic("PORT"),
   },
 };
+
+// TODO: can replace with zod validation
+function getEnvOrPanic(key: string, allowEmptyString = false) {
+  if (
+    !Object.hasOwn(process.env, key) ||
+    (!allowEmptyString && !process.env[key])
+  ) {
+    throw new Error(`> ${key} not set, check your env vars`, {
+      cause: { [key]: process.env[key] },
+    });
+  }
+
+  return process.env[key];
+}
