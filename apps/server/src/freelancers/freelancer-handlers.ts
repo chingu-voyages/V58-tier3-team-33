91c55c8ce6ec8f9a45edb4cb6e505a836c77e1cf
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-12-14 00:27:38 +0100

    feat(server): list freelancers, optionally by job

--- apps/server/src/freelancers/freelancer-handlers.ts
+++ apps/server/src/freelancers/freelancer-handlers.ts
@@ -5,6 +5,67 @@ import { fromNodeHeaders } from "better-auth/node";
 import { auth } from "../auth/index.js";
 import { isValidId, makeDb } from "../database/db.js";
 
+export async function getFreelancers(req: Request, res: Response) {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  });
+
+  if (!session) {
+    res.status(401).json({ message: "log in to continue" });
+    return;
+  }
+
+  const { job: slug } = req.query;
+
+  if (slug && typeof slug != "string") {
+    res.status(422).json({ message: "invalid slug" });
+    return;
+  }
+
+  const normalized = {
+    slug: slug?.trim().toLowerCase(),
+  };
+
+  try {
+    const db = makeDb<{ applications: ApplicationsTable; jobs: JobsTable }>();
+
+    let job;
+    if (normalized.slug) {
+      job = await db
+        .selectFrom("jobs")
+        .selectAll()
+        .where("jobs.slug", "=", normalized.slug)
+        .executeTakeFirst();
+      if (!job) {
+        res.status(422).json({ message: "invalid job" });
+        return;
+      }
+    }
+
+    let freelancersQuery = db
+      .selectFrom("applications")
+      .innerJoin("jobs", "jobs.id", "applications.job_id")
+      .selectAll("applications")
+      .select(["jobs.name as job_name", "jobs.slug as job_slug"])
+      .where("status", "=", "approved");
+
+    if (job) {
+      freelancersQuery = freelancersQuery.where("job_id", "=", job.id);
+    }
+
+    const freelancers = await freelancersQuery.execute();
+    if (!freelancers) {
+      res.status(404).json({ message: "freelancers not found" });
+      return;
+    }
+
+    res.json({ freelancers });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "internal server error" });
+  }
+}
+
 export async function getFreelancerProfile(req: Request, res: Response) {
   const session = await auth.api.getSession({
     headers: fromNodeHeaders(req.headers),
